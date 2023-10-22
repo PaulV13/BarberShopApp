370ac4b455c0af2695428ee5f4f9f1a92e3667c3
paul vidart <paulvidart@gmail.com>
2023-10-22 00:38:36 -0300

    Init commit

---
+++ app/src/main/java/com/example/barbershopapp/ui/register/RegisterFragment.kt
@@ -0,0 +1,23 @@
+package com.example.barbershopapp.ui.register
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.barbershopapp.R
+import com.example.barbershopapp.databinding.FragmentRegisterBinding
+
+class RegisterFragment : Fragment() {
+
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRegisterBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
